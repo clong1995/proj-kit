a54a30113435e9eb412e70caf033d1eb66b30835
QiaoJun <1120530717@qq.com>
2024-07-26 09:06:59 +0800

    修复web下载问题

--- system/lib/func/file_downloader/file_downloader_web.dart
+++ system/lib/func/file_downloader/file_downloader_web.dart
@@ -2,9 +2,12 @@ import 'dart:typed_data';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
-Future<String?> saveFile(Uint8List data, String filename) async{
+Future<String?> saveFile(Uint8List data, String filename) async {
   final blob = html.Blob([data]);
   final url = html.Url.createObjectUrlFromBlob(blob);
+  html.AnchorElement(href: url)
+    ..setAttribute("download", filename)
+    ..click();
   html.Url.revokeObjectUrl(url);
   return null;
 }
