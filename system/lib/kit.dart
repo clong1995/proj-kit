b4ad1f0208c893c249b9bdd3b82a363b5ea31558
huazhimeng <2902151499@qq.com>
2024-08-14 17:33:13 +0800

    窗体控制

--- system/lib/kit.dart
+++ system/lib/kit.dart
@@ -1,3 +1,4 @@
+import 'package:device/device.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kit/def.dart';
@@ -38,17 +39,17 @@ import 'widget/toast.dart';
 import 'widget/tool_bar.dart';
 
 Future<void> kitInit(
-    Iterable<Register Function()> registers, {
-      //请求地址的host，如 https://api.abc.com
-      required String host,
-      required String userAgent,
-      //null:根据屏幕自动适配,
-      //0:不使用适配
-      //数字: 按照指定大小适配
-      double? rpx,
-      //窗体大小
-      Size? windowSize,
-    }) async {
+  Iterable<Register Function()> registers, {
+  //请求地址的host，如 https://api.abc.com
+  required String host,
+  required String userAgent,
+  //null:根据屏幕自动适配,
+  //0:不使用适配
+  //数字: 按照指定大小适配
+  double? rpx,
+  //窗体大小
+  Size? windowSize,
+}) async {
   //系统设置
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarBrightness: Brightness.dark,
@@ -58,9 +59,9 @@ Future<void> kitInit(
 
   //注册包
   Package.register(registers.map((Register Function() element) => () {
-    Register r = element();
-    return reg.Register(r.name, r.packageBuilder);
-  }));
+        Register r = element();
+        return reg.Register(r.name, r.packageBuilder);
+      }));
 
   //设置全局的请求host
   sendHost(host, userAgent);
@@ -68,7 +69,11 @@ Future<void> kitInit(
   //竖屏
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  if (windowSize != null) {
+  if (windowSize != null &&
+      (Device.platform == "windows" ||
+          Device.platform == "macOS" ||
+          Device.platform == "linux" ||
+          Device.platform == "fuchsia")) {
     await windowManager.ensureInitialized();
     await windowManager.waitUntilReadyToShow(
         WindowOptions(
@@ -78,7 +83,6 @@ Future<void> kitInit(
           titleBarStyle: TitleBarStyle.hidden,
           windowButtonVisibility: false,
         ), () async {
-      await windowManager.setResizable(true);
       await windowManager.show();
       await windowManager.focus();
     });
