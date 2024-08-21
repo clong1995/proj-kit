bf652d58a8d39ae286b114e71ad73fb611baff32
QiaoJun <1120530717@qq.com>
2024-08-21 17:52:15 +0800

    文件保存

--- system/lib/func/file_downloader/file_downloader_io.dart
+++ system/lib/func/file_downloader/file_downloader_io.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 
 Future<String?> saveFile(Uint8List data, String filename) async {
-    final directory = await getDownloadsDirectory();
-  final file = File('${directory?.path}/$filename');
+  //final directory = await getDownloadsDirectory();
+  //final file = File('${directory?.path}/$filename');
+  String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+  if (selectedDirectory == null) return null;
+  final file = File('$selectedDirectory/$filename');
   await file.writeAsBytes(data);
   debugPrint('File saved to: ${file.path}');
   return file.path;
