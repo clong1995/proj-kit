eb077302e1a4c58c776b120e3283445254dc49af
QiaoJun <1120530717@qq.com>
2024-10-15 11:02:51 +0800

    修复路径问题

--- system/lib/func/bridge/src/bridge_factory.dart
+++ system/lib/func/bridge/src/bridge_factory.dart
@@ -3,11 +3,11 @@ import 'package:kit/web_bridge.dart';
 
 
 import 'impl/web_bridge_stub.dart'
-    if (dart.library.html) '../impl/win32_bridge.dart'
-    if (dart.library.io) '../impl/web_bridge_stub.dart' as bridge;
+    if (dart.library.html) 'impl/win32_bridge.dart'
+    if (dart.library.io) 'impl/web_bridge_stub.dart' as bridge;
 
 import 'impl/web_bridge_stub.dart'
-    if (dart.library.html) '../impl/chrome_bridge.dart' as bridge2;
+    if (dart.library.html) 'impl/chrome_bridge.dart' as bridge2;
 import 'log_bridge.dart';
 import 'web_bridge_contract.dart';
 
