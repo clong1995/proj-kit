afff0cfea101ec7923082618f3688ba5bf81adb0
QiaoJun <1120530717@qq.com>
2024-12-07 15:05:00 +0800

    拖动问题修复

--- system/lib/widget/drag_bar/win10_drag_bar.dart
+++ system/lib/widget/drag_bar/win10_drag_bar.dart
@@ -3,9 +3,8 @@ import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kit/kit.dart';
-import 'package:system/func/window_manager/window_manager.dart';
 
-
+import '../window_manager/window_manager.dart';
 
 class Win10DragBar extends StatefulWidget {
   final Widget? child;
@@ -19,7 +18,7 @@ class Win10DragBar extends StatefulWidget {
 class _Win10DragBarState extends State<Win10DragBar> {
   Offset start = Offset.zero;
   PointerDeviceKind? kind;
-
+  bool diffEnd = true;
   @override
   void initState() {
     print("win10 drag bar");
@@ -35,6 +34,7 @@ class _Win10DragBarState extends State<Win10DragBar> {
       onPanStart: (d) {
         kind = d.kind;
         print("PointerDeviceKind = ${d.kind}");
+        diffEnd = true;
         // 区别鼠标
         if (kind == PointerDeviceKind.mouse) {
           windowManager.startDragging();
@@ -45,15 +45,20 @@ class _Win10DragBarState extends State<Win10DragBar> {
       },
       onPanUpdate: (d) {
         if (kIsWeb) {
-          if (kind != PointerDeviceKind.mouse) {
-            final diff = d.globalPosition - start;
-            print("pan update offset:${d.globalPosition}, diff:${diff}");
-            kit.webBridge?.moveBy((diff.dx).ceil(), (diff.dy).ceil());
-          } else {
-            //kit.windowManager.startDragging();
-            // print("update start startDragging");
-            print("pan update");
+          if(diffEnd){
+            diffEnd = false;
+            if (kind != PointerDeviceKind.mouse) {
+              Offset diff = d.globalPosition - start;
+              kit.webBridge?.moveBy((diff.dx).ceil(), (diff.dy).ceil()).then((value){
+                diffEnd = true;
+              });
+            } else {
+              //kit.windowManager.startDragging();
+              // print("update start startDragging");
+              print("pan update");
+            }
           }
+
         }
       },
       child: Container(
