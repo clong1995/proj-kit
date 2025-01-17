03cddb7487e670d464073b84f863616b4834f93c
huazhimeng <2902151499@qq.com>
2025-01-17 18:38:40 +0800

    提交

--- system/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ system/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,16 +8,16 @@ import Foundation
 import device_info_plus
 import package_info_plus
 import path_provider_foundation
-import screen_retriever
+import screen_retriever_macos
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
