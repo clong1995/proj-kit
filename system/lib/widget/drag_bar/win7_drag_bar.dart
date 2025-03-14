170e94162aa3ea63fe94456cb9a2bcb350284da1
QiaoJun <1120530717@qq.com>
2025-03-14 10:12:40 +0800

    dragbar  问题修复

--- system/lib/widget/drag_bar/win7_drag_bar.dart
+++ system/lib/widget/drag_bar/win7_drag_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:window_manager/window_manager.dart';
 
+import '../../func/window_manager/window_manager.dart';
 
 class Win7DragBar extends StatefulWidget {
   final Widget? child;
@@ -12,6 +12,20 @@ class Win7DragBar extends StatefulWidget {
 }
 
 class _Win7DragBarState extends State<Win7DragBar> {
+  bool _resetGesture = false;
+
+  void _reset() {
+    setState(() {
+      _resetGesture = true;
+    });
+
+    Future.delayed(Duration(milliseconds: 50), () {
+      setState(() {
+        _resetGesture = false;
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -20,19 +34,32 @@ class _Win7DragBarState extends State<Win7DragBar> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTapCancel: () {
-        },
-        onPanStart: (details) {
-          windowManager.startDragging();
-        },
-        onPanDown: (d) {},
-        onPanUpdate: (d) {
-        },
-        child: Container(
-          color: Colors.transparent,
-          child: widget.child,
-        ));
+    return Listener(
+      onPointerUp: (d){
+        print("onPointerUp");
+        _reset();
+      },
+      child: Stack(
+        children: [
+          _resetGesture
+              ? Container(
+                  color: Colors.transparent,
+                  child: widget.child,
+                )
+              : GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onTapCancel: () {},
+                  onPanStart: (details) {
+                    windowManager.startDragging();
+                  },
+                  onPanDown: (d) {},
+                  onPanUpdate: (d) {},
+                  child: Container(
+                    color: Colors.transparent,
+                    child: widget.child,
+                  ))
+        ],
+      ),
+    );
   }
 }
